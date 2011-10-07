e770f4404ef54ef71985bbeb2ba7b8ac618d7e76
Brandon Keene <bkeene@gmail.com>
2011-10-07 11:03:15 -0400

    Only initialize less when installed

--- lib/twitter-bootstrap-rails/engine.rb
+++ lib/twitter-bootstrap-rails/engine.rb
@@ -2,16 +2,14 @@ module Twitter
   module Bootstrap
     module Rails
       class Engine < ::Rails::Engine
-
         config.after_initialize do |app|
-          twitter_bootstrap_less_files = config.root + 'vendor/stylesheets/bootstrap'
-          app.config.less.paths << twitter_bootstrap_less_files
-
-          puts app.config.less.paths.inspect
+          # Only run when less is installed
+          if app.config.try(:less)
+            twitter_bootstrap_less_files = config.root + 'vendor/stylesheets/bootstrap'
+            app.config.less.paths << twitter_bootstrap_less_files
+          end
         end
-
-	    end
+      end
     end
   end
 end
-
