c08e7c33f92c9cf1aa38cb6469eb99d8bbc0faeb
Seyhun Akyürek <seyhunak@gmail.com>
2011-10-21 04:53:27 +0300

    updated js files

--- vendor/assets/javascripts/bootstrap.js
+++
@@ -1,8 +0,0 @@
-//= require bootstrap-alerts
-//= require bootstrap-dropdown
-//= require bootstrap-modal
-//= require bootstrap-twipsy
-//= require bootstrap-popover
-//= require bootstrap-scrollspy
-//= require bootstrap-tabs
-
