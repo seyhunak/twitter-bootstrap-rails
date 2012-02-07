c64dff41031dfc34ff7c6d301e8c8d473dbb457a
Jérémy CARLIER <carlier.jeremy@gmail.com>
2012-02-07 17:32:47 +0100

    Add the possibility to use the official bootstrap submodule for always keep up-to-date this project

--- lib/twitter/bootstrap/rails/engine.rb
+++ lib/twitter/bootstrap/rails/engine.rb
@@ -4,11 +4,11 @@ module Twitter
   module Bootstrap
     module Rails
       class Engine < ::Rails::Engine
-     
-        initializer 'twitter-bootstrap-rails.setup', 
-          :after => 'less-rails.after.load_config_initializers', 
+
+        initializer 'twitter-bootstrap-rails.setup',
+          :after => 'less-rails.after.load_config_initializers',
           :group => :all do |app|
-            app.config.less.paths << File.join(config.root, 'vendor', 'toolkit')
+            app.config.assets.paths << File.join(config.root, 'vendor', 'bootstrap')
         end
 
       end
