f8fcde6d9e5c83b3c85e96c5247aac3dd77b9dfd
Dat Le <thanhdat.le93@gmail.com>
2012-02-08 12:34:16 -0600

    compile bootstrap_responsive separately as we are supposed to be doing

--- lib/generators/bootstrap/install/install_generator.rb
+++ lib/generators/bootstrap/install/install_generator.rb
@@ -7,17 +7,18 @@ module Bootstrap
       desc "This generator installs Twitter Bootstrap to Asset Pipeline"
 
       def add_assets
-       if File.exist?('app/assets/javascripts/application.js')
-	      insert_into_file "app/assets/javascripts/application.js", "//= require twitter/bootstrap\n", :after => "jquery_ujs\n"
-	     else
-	      copy_file "application.js", "app/assets/javascripts/application.js"
-	     end
-       if File.exist?('app/assets/stylesheets/application.css')
-	      insert_into_file "app/assets/stylesheets/application.css", " *= require twitter/bootstrap\n", :after => "require_self\n"
-	     else
-	      copy_file "application.css", "app/assets/stylesheets/application.css"
-	     end
-  	  end
+        if File.exist?('app/assets/javascripts/application.js')
+          insert_into_file "app/assets/javascripts/application.js", "//= require twitter/bootstrap\n", :after => "jquery_ujs\n"
+        else
+          copy_file "application.js", "app/assets/javascripts/application.js"
+        end
+        if File.exist?('app/assets/stylesheets/application.css')
+          style_require_block = " *= require twitter/bootstrap\n *= require twitter/bootstrap_responsive\n"
+          insert_into_file "app/assets/stylesheets/application.css", style_require_block, :after => "require_self\n"
+        else
+          copy_file "application.css", "app/assets/stylesheets/application.css"
+        end
+      end
 
       def add_bootstrap
         copy_file "bootstrap.coffee", "app/assets/javascripts/bootstrap.js.coffee"
