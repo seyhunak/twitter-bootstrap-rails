e009da957e3077ad2fb8595c948a862816594243
Yovoslav Ivanov <yovoslav.ivanov@gmail.com>
2012-03-07 18:38:14 +0100

    Adds twitter bootstrap breadcrumb support.

--- lib/twitter/bootstrap/rails/engine.rb
+++ lib/twitter/bootstrap/rails/engine.rb
@@ -1,16 +1,22 @@
 require 'rails'
+require File.dirname(__FILE__) + '/twitter-bootstrap-breadcrumbs.rb'
 
 module Twitter
   module Bootstrap
     module Rails
       class Engine < ::Rails::Engine
-     
+
         initializer 'twitter-bootstrap-rails.setup', 
           :after => 'less-rails.after.load_config_initializers', 
           :group => :all do |app|
-            app.config.less.paths << File.join(config.root, 'vendor', 'toolkit')
-        end
+          app.config.less.paths << File.join(config.root, 'vendor', 'toolkit')
+          end
 
+        initializer 'twitter-bootstrap-rails.setup_helpers' do |app|
+          app.config.to_prepare do
+            ActionController::Base.send(:include, BreadCrumbs)
+          end
+        end
       end
     end
   end
