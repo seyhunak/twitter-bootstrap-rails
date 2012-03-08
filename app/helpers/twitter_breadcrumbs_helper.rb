027cb724a5e183d22e9586ce5387f9d527a7181c
Yovoslav Ivanov <yovoslav.ivanov@gmail.com>
2012-03-08 10:05:18 +0100

    Adds default argument on helper rendering method.

--- app/helpers/twitter_breadcrumbs_helper.rb
+++ app/helpers/twitter_breadcrumbs_helper.rb
@@ -1,5 +1,5 @@
 module TwitterBreadcrumbsHelper
-  def render_breadcrumbs(divider)
+  def render_breadcrumbs(divider = '/')
     render partial: 'twitter-bootstrap/breadcrumbs', locals: {divider: divider}
   end
 end
