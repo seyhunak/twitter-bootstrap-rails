031b4aa711aec351d3a3a1ee2f8c70da1a0fb4c6
Oliver Letterer <oliver.letterer@googlemail.com>
2012-05-09 17:15:05 +0200

    Readds twitter-bootstrap-breadcrumbs because of required class methods.

--- lib/twitter/bootstrap/rails/engine.rb
+++ lib/twitter/bootstrap/rails/engine.rb
@@ -1,7 +1,7 @@
 require 'rails'
 
+require File.dirname(__FILE__) + '/twitter-bootstrap-breadcrumbs.rb'
 require File.dirname(__FILE__) + '/../../../../app/helpers/flash_block_helper.rb'
-require File.dirname(__FILE__) + '/../../../../app/helpers/bread_crumbs_helper.rb'
 
 module Twitter
   module Bootstrap
@@ -10,7 +10,9 @@ module Twitter
 
         initializer 'twitter-bootstrap-rails.setup_helpers' do |app|
           app.config.to_prepare do
-            ActionController::Base.send :helper, BreadCrumbsHelper
+            ActionController::Base.send :include, BreadCrumbs
+            ActionController::Base.send :helper_method, :render_breadcrumbs
+
             ActionController::Base.send :helper, FlashBlockHelper
           end
         end
---
+++ lib/twitter/bootstrap/rails/twitter-bootstrap-breadcrumbs.rb
@@ -0,0 +1,30 @@
+module Twitter
+  module Bootstrap
+    module BreadCrumbs
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      module ClassMethods
+        def add_breadcrumb name, url, options={}
+          before_filter options do |controller|
+            controller.send :add_breadcrumb, name, url
+          end
+        end
+      end
+
+      protected
+
+      def add_breadcrumb name, url = ''
+        @breadcrumbs ||= []
+        url = eval(url) if url =~ /_path|_url|@/
+          @breadcrumbs << {:name => name, :url => url}
+      end
+
+      def render_breadcrumbs(divider = '/')
+        s = render :partial => 'twitter-bootstrap/breadcrumbs', :locals => {:divider => divider}
+        s.first
+      end
+    end
+  end
+end
