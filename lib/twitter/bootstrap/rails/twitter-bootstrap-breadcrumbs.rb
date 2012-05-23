f590483b6f82678846218fae8686309985c80c2b
Aleksandar Blagotić <aca.blagotic@gmail.com>
2012-05-24 01:27:57 +0200

    Added options to breadcrumb links, blacklisted backup files in .gitignore

--- lib/twitter/bootstrap/rails/twitter-bootstrap-breadcrumbs.rb
+++ lib/twitter/bootstrap/rails/twitter-bootstrap-breadcrumbs.rb
@@ -15,10 +15,10 @@ module Twitter
 
       protected
 
-      def add_breadcrumb name, url = ''
+      def add_breadcrumb name, url = '', options = {}
         @breadcrumbs ||= []
         url = eval(url) if url =~ /_path|_url|@/
-          @breadcrumbs << {:name => name, :url => url}
+          @breadcrumbs << {:name => name, :url => url, :options = options}
       end
 
       def render_breadcrumbs(divider = '/')
