af52d94d10c2f3f674c6e7110685917b1b9f4503
Ohkubo KOHEI <ohkubo@magician.jp>
2012-09-18 21:30:18 +0900

    GlyphHelper

---
+++ app/helpers/glyph_helper.rb
@@ -0,0 +1,12 @@
+module GlyphHelper
+  # ==== Examples
+  # glyph(:share_alt)
+  # # => <i class="icon-share-alt"></i>
+  # glyph(:lock, :white)
+  # # => <i class="icon-lock icon-white"></i>
+
+  def glyph(*names)
+    content_tag :i, nil, class: names.map{|name| "icon-#{name.to_s.gsub('_','-')}" }
+  end
+end
+
