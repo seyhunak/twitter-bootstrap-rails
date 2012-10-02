0d7badd43c88b161c391ea457608314610acb831
Boris Kuznetsov <achempion@gmail.com>
2012-10-02 12:04:24 +0400

    Update app/helpers/bootstrap_flash_helper.rb
    finished fix

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -4,7 +4,9 @@ module BootstrapFlashHelper
    flash.each do |type, message|
      type = :success if type == :notice
      type = :error   if type == :alert
-     text = content_tag(:div, link_to("&times;", "#", :class => "close", "data-dismiss" => "alert") + message, :class => "alert fade in alert-#{type}")
+     text = content_tag(:div, 
+              content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+              message, :class => "alert fade in alert-#{type}")
      flash_messages << text if message
    end
    flash_messages.join("\n").html_safe
