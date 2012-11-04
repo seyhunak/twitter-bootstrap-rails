74e01552efb144a94d956bbb047155ff3d71d6cb
Todd Baur <todd@toadkicker.com>
2012-11-03 20:01:20 -0700

    navigation helper

---
+++ app/helpers/nav_link_helper.rb
@@ -0,0 +1,13 @@
+module NavLinkHelper
+  def page_links_for_navigation
+    link = Struct.new(:name, :url)
+  end
+
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+end
