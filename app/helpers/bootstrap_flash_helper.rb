c6e25ed679d3cd0f6211aae856dc2bfc7a8da8ef
Boris Kuznetsov <achempion@gmail.com>
2013-01-11 23:26:38 +0400

    Update app/helpers/bootstrap_flash_helper.rb
    normal indents is 2 space

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -1,17 +1,17 @@
 module BootstrapFlashHelper  
   def bootstrap_flash
-   flash_messages = []
-   flash.each do |type, message|
-     # Skip Devise :timeout and :timedout flags
-     next if type == :timeout
-     next if type == :timedout
-     type = :success if type == :notice
-     type = :error   if type == :alert
-     text = content_tag(:div, 
-              content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
-              message, :class => "alert fade in alert-#{type}")
-     flash_messages << text if message
-   end
-   flash_messages.join("\n").html_safe
- end
+    flash_messages = []
+    flash.each do |type, message|
+      # Skip Devise :timeout and :timedout flags
+      next if type == :timeout
+      next if type == :timedout
+      type = :success if type == :notice
+      type = :error   if type == :alert
+      text = content_tag(:div, 
+               content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+               message, :class => "alert fade in alert-#{type}")
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
