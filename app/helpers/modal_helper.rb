ce3a61e279b2c5a3724833b6bd7ab465d44f102d
Todd Baur <todd@toadkicker.com>
2013-04-28 20:14:25 -0400

    Revert "tests and docs for modal helper"
    This reverts commit 9ed6881a292faa4ce1139888d76a8843c7e4706f.

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -1,55 +1,42 @@
 module ModalHelper
+  def modal_dialog(options = {}, escape = true, &block)
+    default_options = {:class => "bootstrap-modal modal"}
+    content_tag :div, nil, options.merge(default_options), escape, &block
+  end
 
-  #modals have a header, a body, a footer for options.
-  def modal_dialog(options = {}, &block)
-    content_tag :div, :id => options[:id], :class => "bootstrap-modal modal hide fade" do
-      modal_header(options[:header]) +
-      modal_body(options[:body]) +
-      modal_footer(options[:footer])
+  def modal_header(options = {}, escape = true, &block)
+    default_options = {:class => 'modal-header'}
+    content_tag :div, nil, options.merge(default_options), escape do
+      raw("<button class=\"close\" data-dismiss=\"modal\">&times;</button>" + capture(&block))
     end
   end
 
-  def modal_header(options = {}, &block)
-    content_tag :div, :class => 'modal-header' do
-      if options[:show_close] 
-        close_button(options[:dismiss]) +
-        content_tag(:h3, options[:title], &block)
-      else
-        content_tag(:h3, options[:title], &block)
-      end	
-    end
+  def modal_body(options = {}, escape = true, &block)
+    default_options = {:class => 'modal-body'}
+    content_tag :div, nil, options.merge(default_options), escape, &block
   end
 
-  def modal_body(options = {}, &block)
-    content_tag :div, options, :class => 'modal-body', &block
-  end
-
-  def modal_footer(options = {}, &block)
-    content_tag :div, options, :class => 'modal-footer', &block
-    end
-
-  def close_button(dismiss)
-    #It doesn't seem to like content_tag, so we do this instead.	
-    raw("<button class=\"close\" data-dismiss=\"#{dismiss}\">&times;</button>")
+  def modal_footer(options = {}, escape = true, &block)
+    default_options = {:class => 'modal-footer'}
+    content_tag :div, nil, options.merge(default_options), escape, &block
   end
 
   def modal_toggle(content_or_options = nil, options = {}, &block)
     if block_given?
       options = content_or_options if content_or_options.is_a?(Hash)
-      default_options = { :class => 'btn', "data-toggle" => "modal", "href" => options[:dialog] }.merge(options)
+      default_options = {:class => 'btn', "data-toggle" => "modal", "href" => options[:dialog]}.merge(options)
 
       content_tag :a, nil, default_options, true, &block
     else
-      default_options = { :class => 'btn', "data-toggle" => "modal", "href" => options[:dialog] }.merge(options)
+      default_options = {:class => 'btn', "data-toggle" => "modal", "href" => options[:dialog]}.merge(options)
       content_tag :a, content_or_options, default_options, true
     end
   end
 
   def modal_cancel_button content, options = {}
-    default_options = { :class => "btn bootstrap-modal-cancel-button" }
+    default_options = {:class => "btn bootstrap-modal-cancel-button"}
 
     content_tag_string "a", content, default_options.merge(options)
   end
-
 end
 
