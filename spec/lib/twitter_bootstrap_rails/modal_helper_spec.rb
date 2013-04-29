e4adbf36a39ecfba091e0e34bf57383f51d5ff3e
Todd Baur <todd@toadkicker.com>
2013-04-29 08:39:28 -0400

    added more coverage

--- spec/lib/twitter_bootstrap_rails/modal_helper_spec.rb
+++ spec/lib/twitter_bootstrap_rails/modal_helper_spec.rb
@@ -9,7 +9,8 @@ include ModalHelper
 describe ModalHelper, :type => :helper do
   header_with_close    = { :show_close => true, :dismiss => 'modal', :title => 'Modal header' }
   header_without_close = { :show_close => false, :title => 'Modal header' }
-  options              = { :header => header_with_close,
+  options              = { :id => "modal",
+												 	 :header => header_with_close,
                            :body   => 'This is the body',
                            :footer => content_tag(:button, 'Save', :class => 'btn')
   }
@@ -26,9 +27,21 @@ describe ModalHelper, :type => :helper do
       modal_header(header_without_close).gsub(/\n/, "").should eql MODAL_HEADER_WITHOUT_CLOSE.gsub(/\n/, "")
   end
 
+	it 'renders a close button' do
+		close_button('modal').should eql "<button class=\"close\" data-dismiss=\"modal\">&times;</button>"
+	end
+
+  it 'renders a modal toggle button' do
+		modal_toggle('Save', :href => "#modal").gsub(/\n/, "").should eql MODAL_TOGGLE.gsub(/\n/, "")
+  end
+
+	it 'renders a cancel button' do
+		modal_cancel_button("Cancel", :href => "#modal", :data => {:dismiss => 'modal'}).gsub(/\n/, "").should eql MODAL_CANCEL_BUTTON.gsub(/\n/, "")
+	end
+
 end
 BASIC_MODAL = <<-HTML
-<div class=\"bootstrap-modal modal\"><div class=\"modal-header\"><button class=\"close\" data-dismiss=\"modal\">&times;</button><h3>Modal header</h3></div><div class=\"modal-body\">This is the body</div><div class=\"modal-footer\"><button class=\"btn\">Save</button></div></div>
+<div class=\"bootstrap-modal modal hide fade\" id="modal"><div class=\"modal-header\"><button class=\"close\" data-dismiss=\"modal\">&times;</button><h3>Modal header</h3></div><div class=\"modal-body\">This is the body</div><div class=\"modal-footer\"><button class=\"btn\">Save</button></div></div>
 HTML
 
 MODAL_HEADER_WITHOUT_CLOSE = <<-HTML
@@ -38,3 +51,12 @@ HTML
 MODAL_HEADER_WITH_CLOSE = <<-HTML
 <div class="modal-header"><button class="close" data-dismiss="modal">&times;</button><h3>Modal header</h3></div>
 HTML
+
+MODAL_TOGGLE = <<-HTML
+<a class="btn" data-toggle="modal" href="#modal">Save</a>
+HTML
+
+MODAL_CANCEL_BUTTON = <<-HTML
+<a class="btn bootstrap-modal-cancel-button" data-dismiss="modal" href="#modal">Cancel</a>
+HTML
+
