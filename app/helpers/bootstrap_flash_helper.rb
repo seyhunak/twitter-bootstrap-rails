1e6adfe799b01a95411e09771d07f3b8537093b1
Shuhei Kagawa <s-kagawa@m3.com>
2014-02-21 16:26:05 +0900

    Show flash messages even if flash type is a string.

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -6,7 +6,8 @@ module BootstrapFlashHelper
     flash.each do |type, message|
       # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
       next if message.blank?
-      
+
+      type = type.to_sym
       type = :success if type == :notice
       type = :danger  if type == :alert
       type = :danger  if type == :error
