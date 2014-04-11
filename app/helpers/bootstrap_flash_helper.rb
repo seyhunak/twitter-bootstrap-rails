0bc00b1f99ee689b922ba737a439d417012d9137
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2014-04-11 09:17:13 -0300

    Fixing bootstrap_flash for Rails 4.1. The 'type' in flash messages is not a Symbol, it needs to be casted.

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -6,7 +6,8 @@ module BootstrapFlashHelper
     flash.each do |type, message|
       # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
       next if message.blank?
-      
+
+      type = type.to_sym
       type = :success if type == :notice
       type = :danger  if type == :alert
       type = :danger  if type == :error
@@ -14,8 +15,8 @@ module BootstrapFlashHelper
 
       Array(message).each do |msg|
         text = content_tag(:div,
-                            content_tag(:button, raw("&times;"), 
-                                       :class => "close", 
+                            content_tag(:button, raw("&times;"),
+                                       :class => "close",
                                        "data-dismiss" => "alert",
                                        "aria-hidden" => "true",
                                        "type" => "button") +
