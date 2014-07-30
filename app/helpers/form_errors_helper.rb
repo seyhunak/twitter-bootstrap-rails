2495482387f9907bd08c56106f3f51d81f4a268a
Todd Baur <todd@toadkicker.com>
2014-07-30 10:41:33 -0700

    moved error handling into a helper. Updated templates to use helper.

---
+++ app/helpers/form_errors_helper.rb
@@ -0,0 +1,18 @@
+module FormErrorsHelper
+  def error_span(attribute, options = {})
+    options[:class] ||= 'help-inline'
+
+    template.content_tag(
+      :span, self.errors_for(attribute),
+      :class => options[:class]
+    ) if self.errors_on?(attribute)
+  end
+
+  def errors_on?(attribute)
+    self.object.errors[attribute].present? if self.object.respond_to?(:errors)
+  end
+
+  def errors_for(attribute)
+    self.object.errors[attribute].try(:join, ', ')
+  end
+end
