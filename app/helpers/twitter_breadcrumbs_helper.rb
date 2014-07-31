229a79bab68d7e1e19e0403a9d32b9066ff528bd
Todd Baur <todd@toadkicker.com>
2014-07-31 14:29:55 -0700

    allow options to be passed into the breadcrumb helper

--- app/helpers/twitter_breadcrumbs_helper.rb
+++ app/helpers/twitter_breadcrumbs_helper.rb
@@ -1,5 +1,6 @@
 module TwitterBreadcrumbsHelper
-  def render_breadcrumbs(divider = '/', &block)
+  def render_breadcrumbs(divider = '/', options={}, &block)
+    default_options = { :class => '', :item_class => '', :divider_class => '' }.merge(options)
     content = render :partial => 'twitter-bootstrap/breadcrumbs', :layout => false, :locals => { :divider => divider }
     if block_given?
       capture(content, &block)
