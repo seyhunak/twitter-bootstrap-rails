4d2768b733467fa2b861258dcd12be73bb27ec59
Todd Baur <todd@toadkicker.com>
2014-07-31 15:00:14 -0700

    syntax fix

--- app/helpers/twitter_breadcrumbs_helper.rb
+++ app/helpers/twitter_breadcrumbs_helper.rb
@@ -1,6 +1,6 @@
 module TwitterBreadcrumbsHelper
   def render_breadcrumbs(divider = '/', options={}, &block)
-    default_options = { :class => '', :item_class => '', :divider_class => '' }.merge(options)
+    default_options = { :class => '', :item_class => '', :divider_class => '', :active_class => 'active' }.merge(options)
     content = render :partial => 'twitter-bootstrap/breadcrumbs', :layout => false, :locals => { :divider => divider, options: options }
     if block_given?
       capture(content, &block)
