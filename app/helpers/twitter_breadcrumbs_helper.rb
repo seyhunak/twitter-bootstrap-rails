0b364a586dd67cb30d3c4e26efc8168a8752bea0
Todd Baur <todd@toadkicker.com>
2014-08-02 11:10:05 -0700

    missed conflict

--- app/helpers/twitter_breadcrumbs_helper.rb
+++ app/helpers/twitter_breadcrumbs_helper.rb
@@ -1,12 +1,7 @@
 module TwitterBreadcrumbsHelper
-<<<<<<< HEAD
-  def render_breadcrumbs(&block)
-    content = render :partial => 'twitter-bootstrap/breadcrumbs', :layout => false
-=======
   def render_breadcrumbs(divider = '/', options={}, &block)
     default_options = { :class => '', :item_class => '', :divider_class => '', :active_class => 'active' }.merge(options)
     content = render :partial => 'twitter-bootstrap/breadcrumbs', :layout => false, :locals => { :divider => divider, options: options }
->>>>>>> c457371fe4c06bd284a97d737a4d50bbcd266a2a
     if block_given?
       capture(content, &block)
     else
