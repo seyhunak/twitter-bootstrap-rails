f1b3b9404d36bc5a51fb9df59187a77815b78d56
Todd Baur <todd@toadkicker.com>
2014-08-11 09:34:36 -0700

    update test to expects syntax

--- spec/lib/breadcrumbs_spec.rb
+++ spec/lib/breadcrumbs_spec.rb
@@ -64,11 +64,11 @@ describe Twitter::Bootstrap::Breadcrumbs do
     [:base, :base_i18n, :class, :class_i18n, :instance, :instance_i18n, :test_model, :symbolized].each do |name|
       path = "/#{name}"
       idx = @controller.breadcrumbs.index { |b| b[:name] == name.to_s && b[:url] == path }
-      idx.should be, -> { name }
+      expect(idx).to be, -> { name }
     end
 
     idx = @controller.breadcrumbs.index { |b| b[:name] == "show" && b[:url] == '' }
-    idx.should be
+    expect(idx).to contain { url == "boat"}
   end
 
 end
