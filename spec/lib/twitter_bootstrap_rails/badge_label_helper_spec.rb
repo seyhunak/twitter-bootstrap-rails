94d8e04ab70847715aa09949bd3bc9855f50d2cd
Todd Baur <todd@toadkicker.com>
2014-08-18 21:14:07 -0700

    added test to check Number can be passed in

--- spec/lib/twitter_bootstrap_rails/badge_label_helper_spec.rb
+++ spec/lib/twitter_bootstrap_rails/badge_label_helper_spec.rb
@@ -10,15 +10,21 @@ include BadgeLabelHelper
 
 describe BadgeLabelHelper, :type => :helper do
   it "should return a basic bootstrap badge" do
-    expect(badge('test').gsub(/\s/, '').downcase)
+    expect(badge('New!').gsub(/\s/, '').downcase)
       .to eql(BASIC_BADGE.gsub(/\s/, '').downcase)
   end
 
   it "should return a bootstrap badge with class" do
-    expect(badge('waning', :warning).gsub(/\s/, '').downcase)
+    expect(badge('2', :warning).gsub(/\s/, '').downcase)
       .to eql(BADGE_WITH_CLASS.gsub(/\s/, '').downcase)
+  end
+
+  it "should take a Number as its first parameter" do
+  	expect(badge(12).gsub(/\s/, '').downcase)
+  	.to eql(BADGE_WITH_NUMBER_PARAM.gsub(/\s/, '').downcase)
   end
 end
 
-BASIC_BADGE = %{<span class="badge">test</span>}
-BADGE_WITH_CLASS = %{<span class="badge badge-warning">waning</span>}
+BASIC_BADGE = %{<span class="badge">New!</span>}
+BADGE_WITH_CLASS = %{<span class="badge badge-warning">2</span>}
+BADGE_WITH_NUMBER_PARAM = %{<span class="badge">12</span>}
