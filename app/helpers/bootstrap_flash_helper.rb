5465fe749f9de51d0bfe9cd6c4157ac2fdd11b91
marco <marco.perrando@gmail.com>
2015-06-12 15:54:18 +0200

    Added the "type" attribute to the dismiss button.
    If an alert is included in a form, the close button could be activated by
    pressing the enter key.
    
    See https://stackoverflow.com/a/11591419

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -18,7 +18,7 @@ module BootstrapFlashHelper
         class: "alert fade in alert-#{type} #{tag_class}"
       }.merge(options)
 
-      close_button = content_tag(:button, raw("&times;"), class: "close", "data-dismiss" => "alert")
+      close_button = content_tag(:button, raw("&times;"), type: "button", class: "close", "data-dismiss" => "alert")
 
       Array(message).each do |msg|
         text = content_tag(:div, close_button + msg.html_safe, tag_options)
