f870d3a3a9223d8e05f29c942826e45bee39337a
panmari <smmuzi@gmail.com>
2015-08-07 15:36:48 +0200

    Not marking msg as html_safe by default.
    This could be misused as attack vector for xss attacks.
    Added two tests for checking the behavior for the two cases if user
    escapes message or not.

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -21,7 +21,7 @@ module BootstrapFlashHelper
       close_button = content_tag(:button, raw("&times;"), type: "button", class: "close", "data-dismiss" => "alert")
 
       Array(message).each do |msg|
-        text = content_tag(:div, close_button + msg.html_safe, tag_options)
+        text = content_tag(:div, close_button + msg, tag_options)
         flash_messages << text if msg
       end
     end
